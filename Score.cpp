8c315db6021277bc5a203c4875e85ed79f1c799b
SnT917 <santiago.narvaezt@udea.edu.co>
2018-06-09 13:16:48 -0500

    Score main class.

--- Score.cpp
+++ Score.cpp
@@ -3,17 +3,17 @@
 
 Score::Score(QGraphicsItem *parent): QGraphicsTextItem(parent){
     // initialize the score to 0
-    score = 0;
+    score = 10;
 
     // draw the text
-//    setPlainText(QString("Score: ") + QString::number(score)); // Score: 0
-//    setDefaultTextColor(Qt::blue);
-//    setFont(QFont("times",16));
+    setPlainText(QString("Score: ") + QString::number(score)); // Score: 0
+    setDefaultTextColor(Qt::white);
+    setFont(QFont("times",16));
 }
 
 void Score::increase(){
-    score++;
-//    setPlainText(QString("Score: ") + QString::number(score)); // Score: 1
+    score-=1;
+    setPlainText(QString("Score: ") + QString::number(score)); // Score: 1
 }
 
 int Score::getScore(){
